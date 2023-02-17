c2ac9e3ef0071d8de18be2f39b4ab096e2f63004
unknown <javiermartinezsoto98@gmail.com>
2023-02-17 16:49:46 -0600

    Initial commit Nails Salon

---
+++ src/main/java/com/mycompany/nailssalon/NailsSalon.java
@@ -0,0 +1,14 @@
+
+package com.mycompany.nailssalon;
+import javax.swing.JOptionPane;
+
+public class NailsSalon {
+
+    public static void main(String[] args) {
+        
+        
+        JOptionPane.showMessageDialog(null,"Nails Salon");
+        
+        
+    }
+}
